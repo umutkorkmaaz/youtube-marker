226f2f2d5b844b0159092cb1613dead287c724a5
Umut Korkmaz <umutkorkmaaz@gmail.com>
2022-07-30 23:08:46 +0300

    save markups to chrome.storage.sync

--- js/content.js
+++ js/content.js
@@ -1,39 +1,106 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let rightControls = document.querySelector(
+  const videoID = new URL(window.location.href).searchParams.get("v");
+  let videoMarkups = getVideoMarkups(videoID);
+  videoMarkups.then((markups) => {
+    console.log(markups);
+  });
+  let videoStatus = false;
+  const video = document.querySelector("video");
+  const rightControls = document.querySelector(
     ".ytp-chrome-controls .ytp-right-controls"
   );
-
-  let svg =
+  const svg =
     '<svg height="100%" viewBox="0 0 24 24" width="75%"><use class="ytp-svg-shadow" href="#ytp-id-8688"></use><path fill="#fff" fill-rule="evenodd" id="ytp-id-8688" d="M17,18V5H7V18L12,15.82L17,18M17,3A2,2 0 0,1 19,5V21L12,18L5,21V5C5,3.89 5.9,3 7,3H17M11,7H13V9H15V11H13V13H11V11H9V9H11V7Z" /></svg>';
-  let button = document.createElement("button");
+  const styles = {
+    input: {
+      display: "block",
+      position: "absolute",
+      width: "200px",
+      bottom: "70px",
+      border: "none",
+      height: "40px",
+      paddingLeft: "10px",
+    }
+  };
+  //right controls button.
+  const button = document.createElement("button");
   button.classList.add("ytp-button");
   button.classList.add("ytp-markup-button");
   button.setAttribute("data-tooltip-target-id", "ytp-markup-button");
   button.setAttribute("aria-label", "Markup");
   button.setAttribute("title", "Markup");
   button.innerHTML = svg;
+  rightControls.prepend(button);
 
-  let inputStyles = {
-    display: "block",
-    position: "absolute",
-    width: "200px",
-    bottom: "70px",
-    border: "none",
-    height: "40px",
-    paddingLeft: "10px",
-  };
+  //context entry area.
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("ytp-markup-wrapper");
+  wrapper.classList.add("hidden");
+  wrapper.style.position = "absolute";
+
+  const input = document.createElement("input");
+  for (let style in styles.input) {
+    input.style[style] = styles.input[style];
+  }
+  input.type = "text";
+  input.classList.add("ytp-markup-input");
+  input.setAttribute("placeholder", "Add Context");
+  input.setAttribute("aria-label", "Add Context");
+  input.setAttribute("title", "Add Context");
+  input.addEventListener("keyup", function (e) {
+    if (e.keyCode === 27) {
+      input.value = "";
+      wrapper.classList.add("hidden");
+      videoState(videoStatus,video);
+    }
+    if (e.keyCode === 13) {
+      const { time, formatted } = getDuration();
+      let text = input.value;
+
+      let markup = {
+        context: text,
+        formatted: formatted,
+        time: time,
+      };
+
+      addMarkup(videoID, markup);
+      input.value = "";
+      wrapper.classList.add("hidden");
+      videoState(videoStatus,video);
+    }
+  });
+
+  wrapper.appendChild(input);
+  document.body.append(wrapper);
 
   document.addEventListener("click", function (e) {
-    let input = button.nextSibling;
-    let video = document.querySelector("video");
+    if (
+      e.target.classList.contains("ytp-markup-button") &&
+      wrapper.classList.contains("hidden")
+    ) {
+      video.pause();
+      videoStatus = video.paused;
+      input.focus();
+      let x = e.clientX;
+      let y = e.clientY;
+      wrapper.style.left = x + "px";
+      wrapper.style.top = y + "px";
+      wrapper.classList.remove("hidden");
+    } else {
+      if (
+        !e.target.classList.contains("ytp-markup-input") &&
+        !e.target.classList.contains("ytp-markup-wrapper")
+      ) {
+        videoState(videoStatus,video);
+        wrapper.classList.add("hidden");
+      }
+    }
+
+    /*
     let videoStatus = video.paused;
     if (
       e.target.classList.contains("ytp-markup-button") &&
-      input.style.display === "none"
     ) {
-      for (let style in inputStyles) {
-        input.style[style] = inputStyles[style];
-      }
       input.focus();
       video.pause();
     } else {
@@ -44,19 +111,8 @@ document.addEventListener("DOMContentLoaded", () => {
         input.style.display = "none";
         videoState(videoStatus);
       }
-    }
+    }*/
   });
-
-  rightControls.prepend(button);
-
-  let input = document.createElement("input");
-  input.type = "text";
-  input.classList.add("ytp-markup-input");
-  input.setAttribute("placeholder", "Add Context");
-  input.setAttribute("aria-label", "Add Context");
-  input.setAttribute("title", "Add Context");
-  input.style.display = "none";
-  button.after(input);
 });
 
 function getDuration() {
@@ -67,15 +123,15 @@ function getDuration() {
     formatted: fancyTimeFormat(time),
   };
 }
-function videoState(status){
-    let video = document.querySelector("video");
-    if(!status){
-        video.play();
-    }else{
-        video.pause();
+function videoState(status,video) {
+  if (!status) {
+    if (video === undefined) {
+      video = document.querySelector("video");
     }
-}
 
+    video.play();
+  }
+}
 function fancyTimeFormat(duration) {
   // Hours, minutes and seconds
   var hrs = ~~(duration / 3600);
@@ -92,4 +148,22 @@ function fancyTimeFormat(duration) {
   ret += "" + mins + ":" + (secs < 10 ? "0" : "");
   ret += "" + secs;
   return ret;
+}
+function addMarkup(id, newMarkup) {
+  chrome.storage.local.get({ [id]: [] }, function (result) {
+    let markupList = result[id];
+    markupList.push(newMarkup);
+    chrome.storage.local.set({ [id]: markupList });
+  });
+}
+function getVideoMarkups(id) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get({ [id]: [] }, function (result) {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      // Pass the data retrieved from storage down the promise chain.
+      resolve(result[id]);
+    });
+  });
 }

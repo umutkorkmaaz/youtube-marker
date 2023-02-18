7e6e30af6ecb9b8f60ae5da877884c5947dea48c
Umut Korkmaz <umutkorkmaaz@gmail.com>
2023-02-19 00:52:31 +0300

    chore: remove console logs and tab indents

--- popup.js
+++ popup.js
@@ -1,70 +1,55 @@
 document.addEventListener('DOMContentLoaded',function(){
-    fetchMarkups().then((response) => {
-        for (const [key, value] of Object.entries(response)){
-            let markup = value.at(0);
-
-            console.log(markup);
-            addVideo({
-                id: key,
-                title: markup.title ?? "Unknown",
-                count: value.length
-            });
-        }
-    });
-
-
-
+  fetchMarkups().then((response) => {
+    for (const [key, value] of Object.entries(response)){
+      let markup = value.at(0);
+      addVideo({
+          id: key,
+          title: markup.title ?? "Unknown",
+          count: value.length
+      });
+    }
+  });
 })
 
-function convertToHTML(string){
-    var parser = new DOMParser();
-	var doc = parser.parseFromString(string, 'text/html');
-	return doc.body;
-}
-
 function addVideo(item){
-    const list = document.getElementById("videolist");
+  const list = document.getElementById("videolist");
 
-    let markupItem = document.createElement('div');
-    markupItem.classList.add("markup-item");
+  let markupItem = document.createElement('div');
+  markupItem.classList.add("markup-item");
 
-    let link = document.createElement('div');
-    link.classList.add("video-link");
-    link.setAttribute("data-url",`https://www.youtube.com/watch?v=${item.id}`);
-    link.innerHTML = `<span class="video-title">${item.title}</span> <span class="markup-count">${item.count}</span>`;
-    markupItem.appendChild(link);
-    link.onclick = () => {
-      open(`https://www.youtube.com/watch?v=${item.id}`)
-    }
+  let link = document.createElement('div');
+  link.classList.add("video-link");
+  link.setAttribute("data-url",`https://www.youtube.com/watch?v=${item.id}`);
+  link.innerHTML = `<span class="video-title">${item.title}</span> <span class="markup-count">${item.count}</span>`;
+  markupItem.appendChild(link);
+  link.onclick = () => {
+    open(`https://www.youtube.com/watch?v=${item.id}`)
+  }
 
-    list.appendChild(markupItem);
+  list.appendChild(markupItem);
 }
+
 function open(url){
-    //let url = e.target.getAttribute("data-url");
-    getCurrentTab().then((tab) => {
-      console.log(tab);
-        chrome.tabs.update(tab.id, { url: url });
-    });
+  getCurrentTab().then((tab) => {
+      chrome.tabs.update(tab.id, { url: url });
+  });
 }
 
 async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-  }
-
-
-
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
 
 function fetchMarkups() {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.get(null, function (result) {
-        if (chrome.runtime.lastError) {
-          return reject(chrome.runtime.lastError);
-        }
-        // Pass the data retrieved from storage down the promise chain.
-        resolve(result);
-      });
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(null, function (result) {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      // Pass the data retrieved from storage down the promise chain.
+      resolve(result);
     });
-  }
+  });
+}
